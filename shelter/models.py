a652ca546a6e233097c87826fde97e555a9699bf
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-03-22 14:33:54 +0100

    Add status to Animal model

--- shelter/models.py
+++ shelter/models.py
@@ -4,20 +4,29 @@ from django.db import models
 # Create your models here.
 class Animal(models.Model):
     SEX = (
-        ('Female', 'Female'),
-        ('Male', 'Male'),
+        ('female', 'female'),
+        ('male', 'male'),
     )
 
     TYPE = (
-        ('Cat', 'Cat'),
-        ('Dog', 'Dog')
+        ('cat', 'cat'),
+        ('dog', 'dog'),
     )
+
+    STATUS = (
+        ('deceased', 'deceased'),
+        ('for adoption', 'for adoption'),
+        ('stay', 'stay'),
+        ('adopted', 'adopted'),
+    )
+
     name = models.CharField(max_length=100, null=True)
     type = models.CharField(max_length=100, null=True, choices=TYPE)
     sex = models.CharField(max_length=100, null=True, choices=SEX)
     daily_ration = models.IntegerField(null=True)
     description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=100, null=True, choices=STATUS)
 
     def __str__(self):
         return self.name
