9c2b70bb16c20e4b2a81e8e3010e6160d1783c3e
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-05-07 18:00:39 +0200

    add class food to models

--- shelter/models.py
+++ shelter/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Animal(models.Model):
     SEX = (
         ('female', 'female'),
@@ -28,6 +27,33 @@ class Animal(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=100, null=True, choices=STATUS)
     animal_pic = models.ImageField(null=True, blank=True)
+    animal_food = models.ManyToManyField('Food')
 
     def __str__(self):
         return self.name
+
+
+class Food(models.Model):
+    FORM = (
+        ('dry', 'dry'),
+        ('wet', 'wet'),
+    )
+
+    LIFE_STAGE = (
+        ('junior', 'junior'),
+        ('adult', 'adult'),
+        ('senior', 'senior'),
+    )
+
+    FEATURE = (
+        ('low fat', 'low fat'),
+        ('bulk', 'bulk'),
+        ('high protein', 'high protein'),
+    )
+
+    name = models.CharField(max_length=128, null=True)
+    form = models.CharField(max_length=128, null=True, choices=FORM)
+    life_stage = models.CharField(max_length=128, null=True, choices=LIFE_STAGE)
+    feature = models.CharField(max_length=128, null=True, choices=FEATURE)
+    weight = models.FloatField(null=True)
+    price = models.FloatField(null=True)
